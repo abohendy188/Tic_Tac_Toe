f4edb4d3c0ca3fd8f9027e1608af6a7e2ef7216c
abohendy188 <abdelrahmanabohendy1882021@gmail.com>
2024-05-03 02:56:12 +0300

    MinMax

--- test.cpp
+++ test.cpp
@@ -12,60 +12,7 @@ void drawBoard(const char symbols[3][3]) {
     cout << "\t\t\t\t\t\t" << " " << symbols[2][0] << " | " << symbols[2][1] << " | " << symbols[2][2] << endl;
     cout << "\n\n\n\n";
 }
-<<<<<<< HEAD
-
-// function to determine the symbol you want to play with
-void chooseSymbol() {
-    char symbol;
-    cout << "Determine the symbol you want to play with: ";
-    cin >> symbol;
-    cin.ignore();
-    if (symbol == 'x' || symbol == 'X') {
-        player_1.playerSymbol = 'X';
-        AI.playerSymbol = 'O';
-        player_2.playerSymbol= 'O';
-    }
-    else {//@Demo You didn't make the case that the user enter a strange letter because you
-    // assume that this code will be on gui and it is only x and o in gui??
-    //Okay, you meant that. I just didn't read the comment below.
-        player_1.playerSymbol = 'O';
-        AI.playerSymbol = 'X';
-        player_2.playerSymbol= 'X';
-    }
-    cout << "\n\n\n";
-}
-
-void mode (int &mode_factor)  // mode_factor is detected upon the user choice to play 2-players mode 
-//or play computer mode
-{
-cout <<"What mode you want to play?"<<endl<<"1- Two players mode"<<endl<<"2- Play with the computer";
-cin>>mode_factor;
-if(mode_factor==1)
-{
-    play_game_2_players();
-    else
-    {
-        play_game_AI();
-    }
-}
-}
-
-
-
-int getSizeOfSymbols(const char symbols[][3]) {
-    int counter = 0;
-    for (int counter_1 = 0; counter_1 < 3; counter_1++)
-        for (int counter_2 = 0; counter_2 < 3; counter_2++)
-            if (symbols[counter_1][counter_2] != ' ')
-                counter++;
-    return counter;
-}
-
-// this function is to check if one of the two opponents has won.
-bool checkWin(const char symbols[][3], char playerSymbol) {
-=======
 int checkWin(const char symbols[][3]) {
->>>>>>> 088ec6e07f8aa7f39df76a3fd0876b73cf693736
     // check the rows
     for (int counter = 0; counter < 3; counter++)
         if (symbols[counter][0] != ' ' && symbols[counter][0] == symbols[counter][1] && symbols[counter][0] == symbols[counter][2])
@@ -93,40 +40,6 @@ int checkWin(const char symbols[][3]) {
     else
         return 1;
 }
-<<<<<<< HEAD
-
-
-// function to get the player's move
-
-int* getPlayerMove() {
-    int* ptr = new int[2]; // this variable is dealed as an array, that will contain the index of the array symbols,
-    // and its length will be 2
-    cout << "Please, enter the index of the move: (1:3 | 1:3): ";
-    cin >> ptr[0] >> ptr[1];
-
-    // here I can make an if condition to check if the index is within the range, 
-    //but u know we are deal with GUI >> to hendo
-    return ptr;
-}
-
-// function to get the computer's move
-int* getComputerMove(const char symbols[][3]) {
-    // here I will not ask to cin >> any thing, as computer will analyze the state of the game, 
-    //and determine the suitable action
-    int counter_1, counter_2;
-    int* ptr = new int[2]; // it is like the pointer in getPlayerMove function
-    // check for winning move
-    for (counter_1 = 0; counter_1 < 3; counter_1++)
-        for (counter_2 = 0; counter_2 < 3; counter_2++)
-            if (symbols[counter_1][counter_2] == ' ') {
-                char tempSymbol[3][3];
-                memcpy(tempSymbol, symbols, sizeof(symbols));
-                tempSymbol[counter_1][counter_2] = AI.playerSymbol;
-                if (checkWin(tempSymbol, AI.playerSymbol)) {
-                    ptr[0] = counter_1;
-                    ptr[1] = counter_2;
-                    return ptr;
-=======
 int miniMax(char symbols[][3], bool isMaximizing, bool firstTime = true) {
     int result = checkWin(symbols);
     if (result != 1) {
@@ -147,7 +60,6 @@ int miniMax(char symbols[][3], bool isMaximizing, bool firstTime = true) {
                         rowIndex = counter_1;
                         colIndex = counter_2;
                     }
->>>>>>> 088ec6e07f8aa7f39df76a3fd0876b73cf693736
                 }
             }
         if (firstTime) {
@@ -184,42 +96,14 @@ void initArray(char symbols[][3]) {
         for (int counter_2 = 0; counter_2 < 3; counter_2++)
             symbols[counter_1][counter_2] = ' ';
 }
-<<<<<<< HEAD
-
-void play_game_2_players()
-{
-
-}
-
-void play_game_AI()
-{
-
-}
-
-
-=======
 // function to play the game
->>>>>>> 088ec6e07f8aa7f39df76a3fd0876b73cf693736
 void playGame() {
     int result = 1;
     int rowIndex = 0;
     int colIndex = 0;
     char symbols[3][3];
-<<<<<<< HEAD
-    bool playerTurn = true;
-    char playAgain;
-    int mode_factor;
-    initialize_grid(symbols);
-    chooseSymbol();
-=======
     initArray(symbols);
->>>>>>> 088ec6e07f8aa7f39df76a3fd0876b73cf693736
     drawBoard(symbols);
-    mode(mode_factor);// This function should have the effect of branching to one of the modes,
-    // and We should define one function for playing every mode 
-
-
-//All written below should be transformed to the function of AI
     while (true) {
         // get player move
         cout << "Enter the row, then enter the column: ";
