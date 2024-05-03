e4594a79553f2c0a6cf441767fa5a044a3c04896
abohendy188 <abdelrahmanabohendy1882021@gmail.com>
2024-05-03 03:59:48 +0300

    some other changes

--- test.cpp
+++ test.cpp
@@ -1,5 +1,5 @@
-//-------------------- file is from ElDemairy, AI task >> to Elsheikh abohendy------------------------------------
-// here, symbol is the main array I used to contain X-O
+//Alsalam Alykom!
+// for more information and notes please go to the readme file!
 
 #include <iostream>
 using namespace std;
@@ -8,9 +8,17 @@ using namespace std;
 //----------------Functions----------------------------------------------------------
 
 
-// function to draw the board
+// 1------- function to initialize the array of symbols by space character(as a sign for being empty).
+void initArray(char symbols[][3]) {
+    for (int counter_1 = 0; counter_1 < 3; counter_1++)
+        for (int counter_2 = 0; counter_2 < 3; counter_2++)
+            symbols[counter_1][counter_2] = ' ';
+}
 
-void drawBoard(const char symbols[3][3]) {
+
+// 2-----function to draw the board
+void drawBoard(const char symbols[3][3])
+ {
     cout << "\t\t\t\t\t\t" << " " << symbols[0][0] << " | " << symbols[0][1] << " | " << symbols[0][2] << endl;
     cout << "\t\t\t\t\t\t" << "-----------" << endl;
     cout << "\t\t\t\t\t\t" << " " << symbols[1][0] << " | " << symbols[1][1] << " | " << symbols[1][2] << endl;
@@ -19,9 +27,19 @@ void drawBoard(const char symbols[3][3]) {
     cout << "\n\n\n\n";
 }
 
-//Function to check the win
+//3-------function to choose the playmode 
+int choose_play_mode()
+{
+    int mode;
+    cout<<"What mode you want to play?"<<endl<<"Enter:\n0 for two players mode\n1 to play with the computer\n";
+    cin>>mode;
+    return mode;
+}
 
-int checkWin(const char symbols[][3]) {
+
+//4------Function to check the win
+int checkWin(const char symbols[][3]) 
+{
     // check the rows
     for (int counter = 0; counter < 3; counter++)
         if (symbols[counter][0] != ' ' && symbols[counter][0] == symbols[counter][1] && symbols[counter][0] == symbols[counter][2])
@@ -33,7 +51,8 @@ int checkWin(const char symbols[][3]) {
             return (symbols[0][counter] == 'X' ? 2 : -2);
 
     // check the diagonal
-    if (symbols[0][0] != ' ' && symbols[0][0] == symbols[1][1] && symbols[0][0] == symbols[2][2]) {
+    if (symbols[0][0] != ' ' && symbols[0][0] == symbols[1][1] && symbols[0][0] == symbols[2][2])
+    {
         return (symbols[0][0] == 'X' ? 2 : -2);
     }
     if (symbols[0][2] != ' ' && symbols[0][2] == symbols[1][1] && symbols[0][2] == symbols[2][0]) {
@@ -49,6 +68,9 @@ int checkWin(const char symbols[][3]) {
     else
         return 1;
 }
+
+//----------------------AI algorithm function-------------------------------------
+
 int miniMax(char symbols[][3], bool isMaximizing, bool firstTime = true) {
     int result = checkWin(symbols);
     if (result != 1) {
@@ -99,22 +121,19 @@ int miniMax(char symbols[][3], bool isMaximizing, bool firstTime = true) {
         return plusInfinity;
     }
 }
-// function to initialize the array of symbols
-void initArray(char symbols[][3]) {
-    for (int counter_1 = 0; counter_1 < 3; counter_1++)
-        for (int counter_2 = 0; counter_2 < 3; counter_2++)
-            symbols[counter_1][counter_2] = ' ';
-}
-// function to play the game
-void playGame() {
-    int result = 1;
-    int rowIndex = 0;
-    int colIndex = 0;
-    char symbols[3][3];
-    initArray(symbols);
-    drawBoard(symbols);
+
+
+//5-------------------functio to Two players mode
+void Two_players_mode(int &rowIndex,int &colIndex,int &result, char symbols[][3])
+{
     
-    while (true) {
+}
+
+//function to play with AI
+
+void play_with_AI(int &rowIndex,int &colIndex,int &result, char symbols[][3])
+{
+ while (true) {
         // get player move
         cout << "Enter the row, then enter the column: ";
         cin >> rowIndex >> colIndex;
@@ -138,6 +157,37 @@ void playGame() {
         }
     }
 }
+
+
+//-----------------------------------------------------------------------------------------------
+
+
+//-----------------Function of the game--------------------------------
+
+
+void playGame() {
+    int result = 1;
+    int rowIndex = 0;
+    int colIndex = 0;
+    char symbols[3][3];
+    initArray(symbols);
+    drawBoard(symbols);
+    if (choose_play_mode())
+    {
+        play_with_AI(rowIndex,colIndex,result,symbols);
+    }
+    else
+    {
+        Two_players_mode(rowIndex,colIndex,result,symbols);
+    }
+   
+   
+}
+
+
+
+//------------------The main function--------------------
+
 int main() {
     cout << "Hello to tic tac toe game!\n";
     cout << "--------------------------\n";
