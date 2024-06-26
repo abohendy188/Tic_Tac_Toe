0aca333893594a2440e789f00dc3f51e747fc30f
Abdelrahman Abohendy <127505027+abohendy188@users.noreply.github.com>
2024-06-26 21:39:50 +0300

    Delete build directory

--- build/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Project_testing/Github/Tic_Tac_Toe/googletest/googlemock
-# Build directory: D:/Project_testing/Github/Tic_Tac_Toe/build/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
--- build/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Project_testing/Github/Tic_Tac_Toe/googletest/googletest
-# Build directory: D:/Project_testing/Github/Tic_Tac_Toe/build/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
