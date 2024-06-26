1e859462c66b517c9678dbf847d664e9e12aa20f
Abdelrahman Abohendy <127505027+abohendy188@users.noreply.github.com>
2024-06-26 23:39:40 +0300

    Update Test_TicTacToe.cpp

--- tests/Test_TicTacToe.cpp
+++ tests/Test_TicTacToe.cpp
@@ -30,32 +30,23 @@ TEST(TicTacToeTest, ResetBoard) {
 
 
 // Test drawing the board
-// TEST(TicTacToeTest, DrawBoard) {
-//     char board[3][3] = {{'X', 'O', 'X'},
-//                         {'O', 'X', 'O'}, 
-//                         {'X', 'O', 'X'}};
-//     testing::internal::CaptureStdout();
-//     drawBoard(board);
-//     std::string output = testing::internal::GetCapturedStdout();
-//     std::string expectedOutput = 
-//         "\f\t\t\t\t\t\t X | O | X\n"
-//         "\t\t\t\t\t\t-----------\n"
-//         "\t\t\t\t\t\t O | X | O\n"
-//         "\t\t\t\t\t\t-----------\n"
-//         "\t\t\t\t\t\t X | O | X\n\n\n\n\n";
-//     EXPECT_EQ(output, expectedOutput);
-// }
-
-extern std::string drawBoard(const char board[3][3]);
-
 TEST(TicTacToeTest, DrawBoard) {
-    char board[3][3] = { {'X', 'O', 'X'},
-                         {'O', 'X', 'O'},
-                         {'X', 'O', 'X'} };
-    std::string output = drawBoard(board);
-    std::string expectedOutput = "\n\t\t\t\t\t\t X | O | X\n\t\t\t\t\t\t-----------\n\t\t\t\t\t\t O | X | O\n\t\t\t\t\t\t-----------\n\t\t\t\t\t\t X | O | X\n\n\n\n\n";
+    char board[3][3] = {{'X', 'O', 'X'},
+                        {'O', 'X', 'O'}, 
+                        {'X', 'O', 'X'}};
+    testing::internal::CaptureStdout();
+    drawBoard(board);
+    std::string output = testing::internal::GetCapturedStdout();
+    std::string expectedOutput = 
+        "\f\t\t\t\t\t\t X | O | X\n"
+        "\t\t\t\t\t\t-----------\n"
+        "\t\t\t\t\t\t O | X | O\n"
+        "\t\t\t\t\t\t-----------\n"
+        "\t\t\t\t\t\t X | O | X\n\n\n\n\n";
     EXPECT_EQ(output, expectedOutput);
 }
+
+
 
 
 
